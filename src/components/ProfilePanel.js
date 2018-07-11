d1205105256a4cf010344d975e4cae68716a9f67
nkhanhng97@gmail.com <khanhmatden123@gmail.com>
2018-07-12 02:15:27 +0700

    login

--- src/components/ProfilePanel.js
+++ src/components/ProfilePanel.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Register from './SignInAndUp/Register';
 import {Button} from 'reactstrap';
+import Login from './SignInAndUp/LoginModal';
 
 class ProfilePanel extends Component {
   state={
+    loginModalOpen : false ,
     regModalOpen : false
   }
 
@@ -13,14 +15,26 @@ class ProfilePanel extends Component {
     })
   }
 
+  login = () => {
+    this.setState({
+      loginModalOpen :true
+    })
+  }
+
   render() {
     return (
       <div className='mr-3'>
-          <Button className="btn btn-light " onClick={this.register}>Register</Button>
+          <Button className="btn btn-light " onClick={this.register}>Sign Up</Button>{'  '}
+          <Button className="btn btn-light " onClick={this.login}>Sign In</Button>
           
           <Register 
             isOpen={this.state.regModalOpen}
             toggle={()=>this.setState({regModalOpen: !this.state.regModalOpen})}
+          />
+
+          <Login
+            isOpen={this.state.loginModalOpen}
+            toggle={()=>this.setState({loginModalOpen: !this.state.loginModalOpen})}
           />
       </div>
     );
