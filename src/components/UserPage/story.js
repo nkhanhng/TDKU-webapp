ee2133f4a14ba2d96239b884a5eec5d6a2dcd858
Kirari Kirisaki <kirisaki124@yahoo.com>
2018-07-15 03:36:40 +0700

    .

--- src/components/UserPage/story.js
+++ src/components/UserPage/story.js
@@ -5,7 +5,7 @@ class Story extends Component {
     return (
       <div className='container'>
         <div className='row'>
-          <img style={{height: '100px'}} src={'http://localhost:6969' + this.props.imageUrl}/>
+          <img style={{height: '100px'}} src={'http://localhost:6969' + this.props.imageUrl} alt='post'/>
           <h1>{this.props.title}</h1>
         </div>
       </div>
